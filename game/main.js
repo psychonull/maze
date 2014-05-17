960675c2bb223f0b1721e5592885624bb6d12eef
Matias Arriola <matias.arriola@gmail.com>
2014-05-17 14:17:18 -0300

    game scaling

--- game/main.js
+++ game/main.js
@@ -2,7 +2,7 @@
 
 //global variables
 window.onload = function () {
-  var game = new Phaser.Game(1000, 700, Phaser.AUTO, 'maze');
+  var game = new Phaser.Game(1000, 700, Phaser.AUTO);
 
   // Game States
   game.state.add('boot', require('./states/boot'));
--- game/states/boot.js
+++ game/states/boot.js
@@ -6,6 +6,15 @@ function Boot() {
 
 Boot.prototype = {
   preload: function() {
+
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.setScreenSize();
+    this.game.scale.startFullScreen(false);
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.pageAlignVeritcally = true;
+    this.game.scale.setShowAll();
+    this.game.scale.refresh();
+
     this.load.image('preloader', 'assets/preloader.gif');
   },
   create: function() {
